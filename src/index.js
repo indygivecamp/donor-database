7da5a8d04bec27750d8ac22954c72f3a44f1447e
Shawn Windler <shawn.windler@centricconsulting.com>
2013-11-02 02:09:00 -0400

    Dynamic content injection for admin pages

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 var DD = {};
 
-$(document).on( 'pagebeforeshow', '#fundraisers', function() {
-	
+
+$(document).on( 'pagebeforeshow', 'div[data-role="page"][data-type="admin"]', function( e, data ) {
+
+	var page = $(e.currentTarget)
+		, path = page.data('path')
+		, id = '#' + page.attr('id')
+		, title = page.data('title')
+		, content = $('#admin-page-content').html()
+	;
+
+	page.html( content );
+	page.find( 'h1' ).html( title );
+	page.find( 'input' ).attr( 'placeholder', title );
+	page.trigger( 'pagecreate' );
+
+	var jqList = $('ul', id);
+
 	// HANDLE ADD NEW FUNDRAISER CLICK
-	$('#fundraisers').off().on( 'click', 'a', function(e) {
-		var val = $('#fundraisers input').val();
+	$( id ).off().on( 'click', 'a', function(e) {
+		var val = $('input', id).val();
 		if( !!val ) {
-			DD.saveNewListItem( 'api/fundraisers', { name: val } );
+			DD.saveNewListItem( path, { name: val }, jqList );
 		}
 	});
 
 	// LOAD DATA
-	DD.getListItems( '/api/fundraisers', $('#fundraisers ul') );
+	DD.getListItems( path, jqList );
 });
+
 
 // LOAD JSON DATA (ID/NAME) AS LIST ITEMS FROM PATH
 DD.getListItems = function( path, jqList ) {
-	return $.getJSON( path, function( data ) {
+	return $.getJSON( '/api/' + path, function( data ) {
 		var results = '';
 		$.each( data, function( index, item ) {
 			results += '<li data-id="' + item.id + '">' + item.name + '</li>';
@@ -27,7 +43,8 @@ DD.getListItems = function( path, jqList ) {
 };
 
 DD.saveNewListItem = function( path, data, jqList ) {
-	return $.post( path, data, function( data ) {
-
+	return $.post( '/api/' + path, data, function( response ) {
+		jqList.prepend( '<li data-id="' + response.id + '">' + data.name + '</li>' );
+		jqList.listview( 'refresh' );
 	});
 };
