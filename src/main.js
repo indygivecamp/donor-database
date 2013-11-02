9606c0e32c279b67802a64fe0c7c8a5f049eb306
AutoSponge <pgrenier@gmail.com>
2013-11-01 23:09:39 -0400

    test harness and linting grunt tasks

---
+++ src/main.js
@@ -0,0 +1,27 @@
+(function (global, a, f) {
+
+    var list = ["map", "filter", "some", "every", "reduce", "reduceRight", "sort"];
+
+    function flip(fn) {
+
+        return function (that, arg) {
+
+            return arguments.length > 1 ? fn.call( that, this, arg ) : fn.call( that, this );
+
+        };
+
+    }
+
+    function createGlobals(fName) {
+
+        global[fName] = f.bind.bind( flip( a[fName] ) );
+        global[fName + "To"] = f.bind.bind( a[fName] );
+
+    }
+
+    global.each = f.bind.bind( flip( a.forEach ) );
+    global.eachTo = f.bind.bind( a.forEach );
+
+    eachTo( list )( createGlobals );
+
+}( this, Array.prototype, Function.prototype ));
