8b7abf33863faddffcc06140b8cfd3910df045ad
joeldart <jdart@remember.com>
2013-11-03 14:07:01 -0500

    fixed the put/post for interest page

--- src/admin.js
+++ src/admin.js
@@ -9,25 +9,28 @@ $(document).on( 'pagebeforeshow', 'div#admin', function( e, data ) {
 		, content = $('#admin-page-content').html()
 	;
 
-	// GRAB DD.Lov and chunk into list items
-	var results = '';
-	$.each( DD.lov, function( name, items ) {
-		var id = name.toLowerCase().replace( / /g, '' );
-		
-		results += '<li><a href="#' + id + '" data-id="' + id + '">' + name + '</a></li>';
-
-        if ( !document.getElementById(id) ) {
-
-            // CREATE NEW PAGE FOR THEM AS WELL
-            $( '<div data-role="page" data-type="admin" data-lovtypeid="' + items.TypeId + '" id="' + id + '" data-title="' + name + '"></div>' )
-                .appendTo( $.mobile.pageContainer ).html( content )
-                .find( 'h1' ).html( name ).end()
-                .find( 'input' ).attr( 'placeholder', name ).end()
-            ;
-
-        }
-	});
-	$( 'ul', page ).html( results ).listview( 'refresh' );
+    
+    DD.promises.admin_lov.done(function () {
+        // GRAB DD.Lov and chunk into list items
+        var results = '';
+        $.each( DD.admin_lov, function( name, items ) {
+            var id = name.toLowerCase().replace( / /g, '' );
+            
+            results += '<li><a href="#' + id + '" data-id="' + id + '">' + name + '</a></li>';
+    
+            if ( !document.getElementById(id) ) {
+    
+                // CREATE NEW PAGE FOR THEM AS WELL
+                $( '<div data-role="page" data-type="admin" data-lovtypeid="' + items[0].LOVTypeID + '" id="' + id + '" data-title="' + name + '"></div>' )
+                    .appendTo( $.mobile.pageContainer ).html( content )
+                    .find( 'h1' ).html( name ).end()
+                    .find( 'input' ).attr( 'placeholder', name ).end()
+                ;
+    
+            }
+        });
+        $( 'ul', page ).html( results ).listview( 'refresh' );
+    });
 });
 
 // INDIVIDUAL ADMIN PAGES
@@ -59,66 +62,70 @@ $(document).on( 'pagebeforeshow', 'div[data-role="page"][data-type="admin"]', fu
         return results;		
 	};
 
-	// LOAD LOV DATA FOR THIS SUBPAGE
-	DD.promises.admin_lov.done(
-		function( data ) {
-			var results = ''
-				, jqFieldSet = $('fieldset', id)
-			;
-			// CREATE ROWS
-			$.each( data[title], function( key, value ) {
-				results += createRow( key, value );
-			});
-            jqFieldSet.html( results ).trigger( 'create' );
-            // ADD HANDLERS FOR 'ADD NEW' AND 'ACTIVE/INACTIVE'
-            $('a[name=addNew]', id).off().on( 'click', function( e ) {
-            	//$.post();
-            	var t = $(this).closest(".inline").find("input[name=new]"),
-            	    val = t.val();
-                
-            	if( !!val ) {
-            		// SAVE NEW TYPE
-            		var newData = {
-            			Name: val
-            			, Active: true
-            			, LOVTypeID: lovTypeID
-            			, DisplayOrder: 1
-            		};
-					$.post( DD.api.lov, JSON.stringify(newData), function( response ) {
-						DD.admin_lov.flush();
-						DD.admin_lov.update().done(
-							function() {
-								page.trigger('create');
-							}
-						);
-					})
-                    .fail(DD.error);
-				}
-            });
+    function createlist ( data ) {
+        var results = ''
+            , jqFieldSet = $('fieldset', id)
+        ;
+        // CREATE ROWS
+        $.each( data[title], function( key, value ) {
+            results += createRow( key, value );
+        });
+        jqFieldSet.html( results ).trigger( 'create' );
+        // ADD HANDLERS FOR 'ADD NEW' AND 'ACTIVE/INACTIVE'
+        $('a[name=addNew]', id).off().on( 'click', function( e ) {
+            //$.post();
+            var t = $(this).closest(".inline").find("input[name=new]"),
+                val = t.val();
+            
+            if( !!val ) {
+                // SAVE NEW TYPE
+                var newData = {
+                    Name: val
+                    , Active: true
+                    , LOVTypeID: lovTypeID
+                    , DisplayOrder: 1
+                };
+                $.ajax( DD.api.lov, {
+                   type: "POST",
+                   contentType: "application/json",
+                   data: JSON.stringify(newData)
+               }).done(function( response ) {
+                    $.publish("lov/flush");
+                    $.publish("lov/update");
+                    $.publish("page/refresh", [ page ]);
+                    DD.promises.admin_lov.done(createlist);
+                })
+                .fail(DD.error);
+            }
+        });
 
-            $('fieldset input[type="checkbox"]', id).off().on( 'change', function( e ) {
-            	var t = $(e.currentTarget)
-            		, id = t.attr('name').split('-')[1]
-            		, lov = data[title][t.data('key')]
-            	;
-            	lov.Active = t.prop( 'checked' );
-            	$.ajax( DD.api.lov + "/" + id, {
-            		type: 'PUT'
-            		, contentType: 'application/json'
-            		, data: JSON.stringify(lov)
-            	}).done(
-            		function() {
-						DD.admin_lov.flush();
-						DD.admin_lov.update().done(
-							function() {
-								page.trigger('create');
-							}
-						);
-            		}
-            	).fail(DD.error);
-            });
-		}
-	);
+        $('fieldset input[type="checkbox"]', id).off().on( 'change', function( e ) {
+            //TODO: currently, disabling an interest will bring down the entire app
+            //since this is not ideal, I'm disabling the ability to do this. when uncommenting
+            //this code, be sure to fix the other issues.
+            /*var t = $(e.currentTarget)
+                , id = t.attr('name').split('-')[1]
+                , lov = data[title][t.data('key')]
+            ;
+            lov.Active = t.prop( 'checked' );
+            $.ajax( DD.api.lov + "/" + id, {
+                type: 'PUT'
+                , contentType: 'application/json'
+                , data: JSON.stringify(lov)
+            }).done(
+                function() {
+                    $.publish("lov/flush");
+                    $.publish("lov/update");
+                    $.publish("page/refresh", [ page ]);
+                }
+            ).fail(DD.error);*/
+        });
+    }
+    
+    
+    
+	// LOAD LOV DATA FOR THIS SUBPAGE
+	DD.promises.admin_lov.done(createlist);
 });
 
 /*$(document).on("click", "#new-button", function() {
