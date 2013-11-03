6aded1dad0ee7f30f1394a692864deeb03e6b8be
Shawn Windler <shawn.windler@centricconsulting.com>
2013-11-03 00:33:45 -0400

    People fixes

--- src/people.js
+++ src/people.js
@@ -13,11 +13,10 @@ $( document ).on( "pagebeforeshow", 'div#people', function( e, data ) {
 			$.each( data, function( index, item ) {
 				console.log( item );
 				var name =
-					( !!item.Title ? item.Title + ' ' : '' ) +
-					( !!item.FirstName ? item.FirstName + ' ' : '' ) +
-					( !!item.LastName ? item.LastName + ' ' : '' ) +
-					( !!item.Suffix ? item.Suffix + ' ' : '' ) +
-					( !!item.OrgName ? '( ' + item.OrgName + ' )' : '' )
+					( !!item.LastName ? item.LastName + ', ' : '' ) +
+					( !!item.FirstName ? item.FirstName : '' ) +
+					( !!item.Suffix ? ', ' + item.Suffix : '' ) +
+					( !!item.OrgName ? ' ( ' + item.OrgName + ' )' : '' )
 				;
 				results += '<li><a href="#" data-uid="' + item.PersonID + '" data-entity="person" class="details">' + name + '</a></li>';
 			});
