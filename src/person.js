42086151513445d77a1c3fe68d8330c5b00642df
AutoSponge <pgrenier@gmail.com>
2013-11-02 22:39:48 -0400

    fix to person data passed on new donation

--- src/person.js
+++ src/person.js
@@ -1,36 +1,5 @@
 var DD = DD || {};
 
-//$(document).on( 'pagebeforeshow', 'div#person', function( e, data ) {
-//
-//    var page = $(e.currentTarget)
-//        , path = page.data('path')
-//        , id = '#' + page.attr('id')
-//        , personType = $("#person-type")
-//    ;
-//
-//    //fetch person
-//    $.getJSON(DD.api.person + "");
-//
-//
-//    //on fetch done create options for person-type
-//    $.each(DD.lov["Person Types"], function (opt) {
-//
-//        var option = "";
-//
-//        option += '<option value="' + opt.id + '"';
-////      make selected
-////        if () {
-////            option += ;
-////        }
-//        option += '>' + opt.displayName;
-//        option += '</option>';
-//
-//        $( option ).appendTo( personType );
-//
-//    });
-//
-//});
-
 $( document).on("click", ".details", function (event) {
 
     var elm = event.target;
@@ -44,6 +13,8 @@ $( document).on("click", ".details", function (event) {
 });
 
 $( document).on("click", "#person-new-donation", function () {
+
+    var person = DD.person[$(this).data("uid")];
 
     $.publish("donation/load", [{}, person]);
 
@@ -97,6 +68,8 @@ $( window ).on( "pagechange", function (event, data) {
         DD.person = DD.person || {};
 
         DD.person[person.PersonID] = person;
+
+        $("#person-new-donation").data("uid", person.PersonID);
 
         $.each(DD.lov["Person Types"], function (i, opt) {
             var option = "";
